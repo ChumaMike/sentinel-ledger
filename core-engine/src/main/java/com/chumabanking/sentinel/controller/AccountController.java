1e1a542ae9509d01c935b96b49115d0bc7207f40
Chuma <chmeyisjhb024@student.wethinkcode.co.za>
2026-01-02 15:29:25 +0200

    feat: transfer logic and account endpoints

--- core-engine/src/main/java/com/chumabanking/sentinel/controller/AccountController.java
+++ core-engine/src/main/java/com/chumabanking/sentinel/controller/AccountController.java
@@ -4,7 +4,7 @@ import com.chumabanking.sentinel.model.Account;
 import com.chumabanking.sentinel.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import com.chumabanking.sentinel.service.AccountService;
 import java.util.List;
 
 @RestController
@@ -22,5 +22,11 @@ public class AccountController {
     @PostMapping
     public Account createAccount(@RequestBody Account account) {
         return accountRepository.save(account);
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(@RequestParam Long fromId, @RequestParam Long toId, @RequestParam java.math.BigDecimal amount) {
+        AccountService.transferMoney(fromId, toId, amount);
+        return "Transfer successful! Sent " + amount + " to Account ID: " + toId;
     }
 }
